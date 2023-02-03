a46d3f75222596d8e61dd271e3ad118a11501030
Matthieu <reungoat.matthieu@gmail.com>
2023-02-03 13:56:08 +0100

    parsing variables from url

--- index.js
+++ index.js
@@ -3,27 +3,6 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-//////////////////////////////////////
-// FILES
-
-//Blocking synchronous way
-/*
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
-const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
-console.log(textOut);
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written!");
-*/
-
-//Non-blocking asynchronous way
-/*
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  console.log(data);
-});
-console.log("Will read file");
-*/
-
 /////////////////////////////////
 // SERVER
 const replaceTemplate = (temp, product) => {
@@ -58,10 +37,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // OVERVIEW PAGE
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -72,12 +51,16 @@ const server = http.createServer((req, res) => {
   }
 
   // PRODUCT PAGE
-  else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
+  else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
   }
 
   // API
-  else if (pathName === "/api") {
+  else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
   }
